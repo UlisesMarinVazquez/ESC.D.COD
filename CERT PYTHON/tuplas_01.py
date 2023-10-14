394ba01f906a231112403b8b2c2a80203e46c5b5
UlisesMarinVazquez <ulisesmavz@gmail.com>
2023-10-14 14:05:10 -0600

    EDIT

---
+++ CERT PYTHON/tuplas_01.py
@@ -0,0 +1,27 @@
+
+
+#Tuplas: No son modificables sólo puedes hacer busquedas en ellas.
+
+frutas=("Manzana", "fresa","Pera",["Mango","Sandia"])
+print(frutas)
+print(frutas[3])
+print(frutas[1:2])
+print(type(frutas))
+lista =[1,2,3]
+print(lista)
+lista.pop(0)
+print(lista)
+print(lista(frutas))
+
+
+
+
+
+
+
+
+
+
+
+
+
